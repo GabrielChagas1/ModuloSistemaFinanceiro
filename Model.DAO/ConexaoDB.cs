bb0518be4ab9f6e43b3f30ec8653c9150c31fe9b
Gabriel Chagas <gabrielserq@outlook.com>
2019-12-16 13:44:33 -0300

    Adicionar arquivos de projeto.

---
+++ Model.DAO/ConexaoDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private SqlConnection con;
+
+        private ConexaoDB()
+        {
+            con = new SqlConnection("Data Source=LAPTOP-ELNKGOCG\\SQLEXPRESS; Initial Catalog=Financeiro; Intregrated Security=True");
+        }
+
+        public static ConexaoDB saberEstado()
+        {
+            if(objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+
+            return objConexaoDB;
+        }
+
+        //Método para pegar a conexão
+        public SqlConnection GetConnection()
+        {
+            return con;
+        }
+
+        //Método para fechar a conexão
+        public void CloseDB()
+        {
+            objConexaoDB = null;
+        }
+    }
+}
