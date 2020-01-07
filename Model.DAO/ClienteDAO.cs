83b7da49d1dd21409019a54aeaa0fc86b148e7fb
Gabriel Chagas <gabrielserq@outlook.com>
2020-01-07 11:34:32 -0300

    finish client DAO

--- Model.DAO/ClienteDAO.cs
+++ Model.DAO/ClienteDAO.cs
@@ -23,8 +23,8 @@ namespace Model.DAO
             string Create = $"INSERT INTO cliente(nome, endereco, telefone, cpf ) VALUES ({objCliente.Nome}, {objCliente.Endereco}, {objCliente.Telefone}, {objCliente.CPF})";
             try
             {
-                Comando = new SqlCommand(Create, objConexaoDB.GetConnection());
-                objConexaoDB.GetConnection().Open();
+                Comando = new SqlCommand(Create, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
                 Comando.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -44,8 +44,8 @@ namespace Model.DAO
             string Create = $"sp_cliente_adc {objCliente.Nome}, {objCliente.Endereco}, {objCliente.Telefone}, {objCliente.CPF}";
             try
             {
-                Comando = new SqlCommand(Create, objConexaoDB.GetConnection());
-                objConexaoDB.GetConnection().Open();
+                Comando = new SqlCommand(Create, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
                 Comando.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -64,8 +64,8 @@ namespace Model.DAO
             string Delete = $"DELETE FROM cliente WHERE idCliente = {objCliente.IdCliente}";
             try
             {
-                Comando = new SqlCommand(Delete, objConexaoDB.GetConnection());
-                objConexaoDB.GetConnection().Open();
+                Comando = new SqlCommand(Delete, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
                 Comando.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -85,8 +85,8 @@ namespace Model.DAO
             string Find = $"SELECT * FROM cliente WHERE idCliente = {objCliente.IdCliente}";
             try
             {
-                Comando = new SqlCommand(Find, objConexaoDB.GetConnection());
-                objConexaoDB.GetConnection().Open();
+                Comando = new SqlCommand(Find, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
                 SqlDataReader reader = Comando.ExecuteReader();
                 IsRecord = reader.Read();
                 if (IsRecord)
@@ -120,8 +120,8 @@ namespace Model.DAO
             string FindAll = "SELECT * FROM cliente ORDER BY nome ASC";
             try
             {
-                Comando = new SqlCommand(FindAll, objConexaoDB.GetConnection());
-                objConexaoDB.GetConnection().Open();
+                Comando = new SqlCommand(FindAll, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
                 SqlDataReader reader = Comando.ExecuteReader();
                 while (reader.Read())
                 {
@@ -147,9 +147,101 @@ namespace Model.DAO
             return listaCliente;
         }
 
-        public void Update(Cliente obj)
+        public void Update(Cliente objCliente)
         {
-            throw new NotImplementedException();
+            string Update = $"UPDATE cliente SET nome = {objCliente.Nome}, endereco = {objCliente.Endereco}, telefone = {objCliente.Telefone}, cpf = {objCliente.CPF}";
+            try
+            {
+                Comando = new SqlCommand(Update, objConexaoDB.GetConnectionnection());
+                objConexaoDB.GetConnectionnection().Open();
+                Comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+                objCliente.Estado = 1;
+            }
+            finally
+            {
+                objConexaoDB.CloseDB();
+            }
+        }
+        //OUTRAS IMPLEMENTAÇÕES
+
+        public bool findClientePorcpf(Cliente objCliente)
+        {
+            bool temRegistros;
+            string find = "select*from cliente where cpf='" + objCliente.CPF + "'";
+            try
+            {
+                Comando = new SqlCommand(find, objConexaoDB.GetConnection());
+                objConexaoDB.GetConnection().Open();
+
+                SqlDataReader reader = Comando.ExecuteReader();
+                temRegistros = reader.Read();
+                if (temRegistros)
+                {
+                    objCliente.Nome = reader[1].ToString();
+                    objCliente.Endereco = reader[2].ToString();
+                    objCliente.Telefone = reader[3].ToString();
+                    objCliente.CPF = reader[4].ToString();
+
+                    objCliente.Estado = 99;
+
+                }
+                else
+                {
+                    objCliente.Estado = 1;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                objConexaoDB.GetConnection().Close();
+                objConexaoDB.CloseDB();
+            }
+            return temRegistros;
+        }
+
+        public List<Cliente> findAllCliente(Cliente objCLiente)
+        {
+            List<Cliente> listaClientes = new List<Cliente>();
+            string findAll = "select* from cliente where nome like '%" + objCLiente.Nome + "%' or cpf like '%" + objCLiente.CPF + "%' or idCliente like '%" + objCLiente.IdCliente + "%' ";
+            try
+            {
+
+                Comando = new SqlCommand(findAll, objConexaoDB.GetConnection());
+                objConexaoDB.GetConnection().Open();
+                SqlDataReader reader = Comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    Cliente objCliente = new Cliente();
+                    objCliente.IdCliente = Convert.ToInt64(reader[0].ToString());
+                    objCliente.Nome = reader[1].ToString();
+
+                    objCliente.Endereco = reader[2].ToString();
+                    objCliente.Telefone = reader[3].ToString();
+                    objCliente.CPF = reader[4].ToString();
+                    listaClientes.Add(objCliente);
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                objConexaoDB.GetConnection().Close();
+                objConexaoDB.CloseDB();
+            }
+
+            return listaClientes;
+
         }
     }
 }
